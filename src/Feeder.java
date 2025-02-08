f025c94ff078da442ab86775eea8382ff70a2952
Matthew <Matirix@live.com>
2025-02-08 08:31:56 -0800

    Updated Question #1

--- src/Feeder.java
+++ src/Feeder.java
@@ -1,27 +1,45 @@
 
-public class Feeder
-{
 /**
-* The amount of food, in grams, currently in the bird feeder; initialized in the constructor and
-* always greater than or equal to zero
-* https://apcentral.collegeboard.org/media/pdf/ap24-frq-comp-sci-a.pdf 
-*/
-private int currentFood;
-/**
-* Simulates one day with numBirds birds or possibly a bear at the bird feeder,
-* as described in part (a)
-* Precondition: numBirds > 0
-*/
-public void simulateOneDay(int numBirds) {
-	{ /* to be implemented in part (a) */ }
+ * AP CSA 2024 Written Question 1
+ * https://apcentral.collegeboard.org/media/pdf/ap24-frq-comp-sci-a.pdf 
+ * PLEASE READ THE PROMPT CAREFULLY BEFORE ATTEMPTING
+ * Also make sure you are on your own branch before pushing, you can check what branch you are on by doing 'git branch -a'
+ */
+public class Feeder {
 	/**
-	* Returns the number of days birds or a bear found food to eat at the feeder in this simulation,
-	* as described in part (b)
-	* Preconditions: numBirds > 0, numDays > 0
+	* The amount of food, in grams, currently in the bird feeder; initialized in the constructor and
+	* always greater than or equal to zero
 	*/
-}
-public int simulateManyDays(int numBirds, int numDays) {
-	{ /* to be implemented in part (b) */ }
-	return 1;
+	private int currentFood;
+	
+	
+	/**
+	 * TODO Implement the Constructor
+	 * @param currentFood
+	 */
+	public Feeder(int currentFood) {
+
 	}
+	
+	/**
+	* Simulates one day with numBirds birds or possibly a bear at the bird feeder,
+	* as described in part (a)
+	* Precondition: numBirds > 0
+	*/
+	public void simulateOneDay(int numBirds) {
+		/**
+		* PART A TODO
+		* Returns the number of days birds or a bear found food to eat at the feeder in this simulation,
+		* as described in part (b)
+		* Preconditions: numBirds > 0, numDays > 0
+		*/
+	}
+	public int simulateManyDays(int numBirds, int numDays) {
+		/**
+		 * TODO PART B:
+		 * Simulates many days SimulateOne day is called.
+		 */
+		
+		return 1;
+		}
 }
